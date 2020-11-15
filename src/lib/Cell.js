d06ffd7299f41c1eee571e1652518f2942fac6b3
ksugimori <ksugimori.private@gmail.com>
2020-11-15 22:30:42 +0900

    Cell#isFlagged を追加

--- src/lib/Cell.js
+++ src/lib/Cell.js
@@ -9,6 +9,7 @@ class Cell {
     this.count = 0;
     this.isOpen = false;
     this.isMine = false;
+    this.isFlagged = false;
 
     Object.assign(this, params);
   }
@@ -25,6 +26,20 @@ class Cell {
    */
   mine() {
     this.isMine = true;
+  }
+
+  /**
+   * フラグを立てる
+   */
+  flag() {
+    this.isFlagged = true;
+  }
+
+  /**
+   * フラグを外す
+   */
+  unflag() {
+    this.isFlagged = false;
   }
 }
 

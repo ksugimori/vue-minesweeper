1e8d50d50e683f77bcd4d37bcb8aa9b3bbf0fcd2
ksugimori <ksugimori.private@gmail.com>
2021-01-04 22:53:00 +0900

    name を Getter に変更

--- tests/unit/lib/setting.spec.js
+++ tests/unit/lib/setting.spec.js
@@ -2,20 +2,44 @@ import Setting from '@/lib/Setting'
 
 describe('Setting', () => {
 
-  it("全項目が一致する場合のみ同値と判定されること", () => {
-    let setting = new Setting("original", 9, 9, 10);
+  describe('#equals', () => {
+    it("全項目が一致する場合のみ同値と判定されること", () => {
+      let setting = new Setting(9, 9, 10);
 
-    let same = new Setting("same", 9, 9, 10);
-    expect(setting.equals(same)).toBeTruthy();
+      let same = new Setting(9, 9, 10);
+      expect(setting.equals(same)).toBeTruthy();
 
-    let diffWidth = new Setting("different width", 10, 9, 10);
-    expect(setting.equals(diffWidth)).toBeFalsy();
+      let diffWidth = new Setting(10, 9, 10);
+      expect(setting.equals(diffWidth)).toBeFalsy();
 
-    let diffHeight = new Setting("different height", 9, 10, 10);
-    expect(setting.equals(diffHeight)).toBeFalsy();
+      let diffHeight = new Setting(9, 10, 10);
+      expect(setting.equals(diffHeight)).toBeFalsy();
 
-    let diffMines = new Setting("different numMines", 9, 9, 11);
-    expect(setting.equals(diffMines)).toBeFalsy();
+      let diffMines = new Setting(9, 9, 11);
+      expect(setting.equals(diffMines)).toBeFalsy();
+    })
   })
 
+  describe('#name', () => {
+    it('EASY と等しい場合は EASY が返ること', () => {
+      let setting = Setting.EASY.clone();
+      expect(setting.name).toBe('EASY');
+    })
+
+    it('NORMAL と等しい場合は NORMAL が返ること', () => {
+      let setting = Setting.NORMAL.clone();
+      expect(setting.name).toBe('NORMAL');
+    })
+
+    it('HARD と等しい場合は HARD が返ること', () => {
+      let setting = Setting.HARD.clone();
+      expect(setting.name).toBe('HARD');
+    })
+
+    it('その他の場合は CUSTOM が返ること', () => {
+      let setting = Setting.NORMAL.clone();
+      setting.width++;
+      expect(setting.name).toBe('CUSTOM');
+    })
+  })
 })
